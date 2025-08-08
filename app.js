008103584a6e89034b683154c60dc3a9676ab186
Gonzalo Ortiz <gonzalogeo@gmail.com>
2025-08-08 00:28:50 -0300

    Proyecto listo para entregar

--- app.js
+++ app.js
@@ -9,9 +9,36 @@ function agregarAmigo (){
         return;
     }
 
-
     amigos.push(nombreAmigo)
     console.log(amigos);//para verificar que se están añadiendo correctamente
 
     document.getElementById('nombreAmigo').value = ''
+
+    actualizarListaAmigos()
+}
+
+function actualizarListaAmigos(){
+
+    let lista = document.getElementById('listaAmigos');
+
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        let amigo = amigos[i]
+
+        let elementoLista = document.createElement('Li');
+        elementoLista.textContent = amigo;
+
+        lista.appendChild
+    }
+}
+
+function sortearAmigo(){
+    if (amigos.length > 0){
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[indiceAleatorio];
+        document.getElementById("resultado").innerHTML = "El amigo sorteado es: " + amigoSorteado
+    } else {
+        document.getElementById("resultado").innerHTML = "No hay amigos para sortear.";
+    }
 }
